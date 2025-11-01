203f4a9a67cef13906e31133ca8d684fa0c90bc7
IamAhmedZaki <mahmedzaki670@gmail.com>
2025-11-01 20:26:30 +0500

    updated

--- src/controllers/sendEmail.controller.js
+++ src/controllers/sendEmail.controller.js
@@ -3217,7 +3217,7 @@ const sendCapEmail = async (req, res) => {
 
     const mailOptions = {
       from: process.env.EMAIL_FROM || process.env.EMAIL_USER,
-      to: 'abdulahad010274@gmail.com',
+      to: email,
       subject: emailContent.subject,
       html: emailContent.html,
       text: emailContent.text
@@ -3225,7 +3225,7 @@ const sendCapEmail = async (req, res) => {
 
     const mailOptionsAdmin = {
       from: process.env.EMAIL_FROM || process.env.EMAIL_USER,
-      to:  'abdulahad010274@gmail.com',
+      to:  'salg@studentlife.dk',
       subject: emailContentAdmin.subject,
       html: emailContentAdmin.html,
       text: emailContentAdmin.text
@@ -3233,7 +3233,7 @@ const sendCapEmail = async (req, res) => {
 
     const mailOptionsFactory = {
       from: process.env.EMAIL_FROM || process.env.EMAIL_USER,
-      to:  'abdulahad010274@gmail.com',
+      to:  'salg@studentlife.dk',
       subject: emailContentFactory.subject,
       html: emailContentFactory.html,
       text: emailContentFactory.text
